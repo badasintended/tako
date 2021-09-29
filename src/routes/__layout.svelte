92add9e69264dfae540944f218db077423e8ea9e
deirn <deirn@bai.lol>
2021-09-29 21:13:26 +0700

    ui: library

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,7 @@
   import { darkMode } from "tako/stores";
   import { onMount } from "svelte";
   import NavBar from "tako/component/navbar/NavBar.svelte";
+  import { page } from "$app/stores";
 
   let showScrollBar = false;
   let mouseX = 0;
@@ -10,6 +11,8 @@
   let msSinceLastScroll = 0;
 
   onMount(() => {
+    document.title = "tako";
+
     darkMode.set(localStorage.getItem("darkMode") === "true");
     darkMode.subscribe(value => localStorage.setItem("darkMode", value ? "true" : "false"));
 
@@ -32,13 +35,15 @@
 <div class:dark={$darkMode}>
   <div class="w-full h-screen flex flex-col flex-auto bottom-0 dark:bg-one-b transition-colors duration-500 ease-out text-gray-500 dark:text-one-w">
     <NavBar />
-    <div class="scroller flex-grow w-full overflow-y-auto overflow-y-auto"
-         class:scroller-hidden={!showScrollBar}
-         on:scroll={onScroll}
-         on:mousemove={onMouse}>
-      <div class="w-full max-w-6xl mx-auto font-sans">
-        <slot />
+    {#key $page.path}
+      <div class="scroller flex-grow w-full overflow-y-auto overflow-y-scroll"
+           class:scroller-hidden={!showScrollBar}
+           on:scroll={onScroll}
+           on:mousemove={onMouse}>
+        <div class="w-full max-w-6xl mx-auto font-sans">
+          <slot />
+        </div>
       </div>
-    </div>
+    {/key}
   </div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,39 @@
 <script lang="ts">
+  import type { Bookmark } from "tako/database";
+  import { database } from "tako/database";
+  import { onMount } from "svelte";
 
+  let bookmarks: Bookmark[] = [];
+
+  onMount(() => {
+    database.libraries
+      .toArray()
+      .then(it => bookmarks = it.sort((a, b) => a.title.localeCompare(b.title)));
+  });
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="pt-2 mb-4">
+  <div class="flex flex-wrap justify-center">
+    {#each bookmarks as bookmark}
+      <a class="flex flex-col w-40 h-72 m-2 group"
+         href="/{bookmark.sourceId}/{bookmark.mangaId}">
+        <img class="flex-shrink-0 w-40 h-60 object-cover rounded-md transition-transform duration-300 ease-in-out group-hover:scale-105"
+             src={bookmark.cover}
+             alt={bookmark.title} />
+        <div class="text-sm title">
+          {bookmark.title}
+        </div>
+      </a>
+    {/each}
+  </div>
+</div>
+
+<style lang="scss">
+  .title {
+    height: 3em;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+</style>
